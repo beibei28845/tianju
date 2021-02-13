17ae3e5e90be814faccee7f8f2640a758851142e
beibei28845 <1097109986@qq.com>
2021-02-13 16:13:53 +0000

    再次确认

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container">
     <p class="float-left">
-      由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">郑迎哲</a> 设计和编码 <span style="color: #1013db;font-size: 14px;">❤</span>
+      由 <a href="http://baidu.com" target="_blank">郑迎哲</a> 设计和编码 <span style="color: #1013db;font-size: 14px;">❤</span>
     </p>
 
     <p class="float-right"><a href="mailto:1097109986@qq.com">联系我们</a></p>
