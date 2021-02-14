3b4bafd0bc29335eac72353af549f8cfaba3b132
beibei28845 <1097109986@qq.com>
2021-02-14 08:10:26 +0000

    找回密码成功消息提示

--- resources/views/shared/_messages.blade.php
+++ resources/views/shared/_messages.blade.php
@@ -1,4 +1,11 @@
 @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+    @if (session()->has($msg))
+        <div class="flash-message">
+            <p class="alert alert-{{ $msg }}">
+                {{ session()->get($msg) }}
+            </p>
+        </div>
+    @endif
     @if (session('success'))
         <div class="alert alert-success" role="alert">
             {{ session('success') }}
