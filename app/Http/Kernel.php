66f7556c8bd67c11f15d91f038da8be3f46529b3
beibei28845 <1097109986@qq.com>
2021-02-14 04:45:15 +0000

    强制用户认证

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,11 +37,12 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EnsureEmailIsVerified::class,
         ],
 
         'api' => [
-            'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'throttle:60,1',
+            'bindings',
         ],
     ];
 
